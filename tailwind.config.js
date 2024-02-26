f850bd954e53a0abe95c5fe1cf5f1b9a3e611567
Emmanuel Esan <nikolasdevs@gmail.com>
2024-02-26 10:19:28 +0100

    new images added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config}, */
 
 export default {
-
-
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: true, // or 'media' or 'class'
   theme: {
     extend: {
+      animation: {
+        "infinite-scroll": "infinite-scroll 25s linear infinite",
+      },
+      keyframes: {
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+      },
       backgroundImage: {
         "hero-section": " url('/src/assets/images/hero-1.jpg')",
       },
