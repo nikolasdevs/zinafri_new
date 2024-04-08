b348052313ce87382ed8713a46a3f2dc5ad78fb0
Emmanuel Esan <nikolasdevs@gmail.com>
2024-04-08 20:26:04 +0100

    service section was redesigned

--- src/components/sections/ServiceSection.vue
+++ src/components/sections/ServiceSection.vue
@@ -42,41 +42,41 @@ export default {
         {
           title: "Quota Management",
           content:
-            "Learn about quota system that controls the number of foreign nationals allowed to enter, work, or reside in Nigeria...",
+            "Learn about quota system that controls the number of foreign nationals allowed to enter, work, or reside in Nigeria",
           id: "quota",
         },
 
         {
           title: "Work Authorization",
           content:
-            "Learn about work authorization issued that allows foreign nationals to legally work and reside in Nigeria... ",
+            "Learn about work authorization issued that allows foreign nationals to legally work and reside in Nigeria ",
           id: "work-auth",
         },
 
         {
           title: "Business",
           content:
-            "Learn about business visa that allows foreign nationals only do business in Nigeria and allows Visa on Arrival ...",
+            "Learn about business visa that allows foreign nationals only do business in Nigeria and allows Visa on Arrival",
           id: "business",
         },
 
         {
           title: "Audit and Reviews",
           content:
-            "A comprehensive review, examination, or assessment carried out to ensure compliance with immigration laws, regulations, and policies...",
+            "A comprehensive review, examination, or assessment carried out to ensure compliance with immigration laws, regulations, and policies",
           id: "audit",
         },
 
         {
           title: "Training",
           content:
-            "Zinafri provides trainings and workshops for individuals, professionals or organizations to provide them with knowledge and skills related to immigration laws...",
+            "Zinafri provides trainings and workshops for individuals and organizations to provide them with knowledge and skills related to immigration laws",
           id: "training",
         },
         {
           title: "Consulting",
           content:
-            "Zinafri provides Immigration consulting  which involves providing guidance, advice, and assistance to individuals, families, businesses, or organizations ...",
+            "Zinafri provides Immigration consulting  which involves providing guidance, advice, and assistance to individuals, families, businesses, or organizations",
           id: "consulting",
         },
       ],
@@ -86,7 +86,7 @@ export default {
 </script>
 
 <template>
-  <div class="service flex items-center justify-center max-w-4xl mx-auto">
+  <div class="service flex items-center justify-center max-w-6xl mx-auto">
     <div
       class="container flex flex-col md:mt-10 mt-16 items-center justify-center px-4"
     >
@@ -100,25 +100,6 @@ export default {
         <div v-for="(card, index) in cardData" :key="index">
           <ServiceCard :title="card.title" :content="card.content">
             <!-- Additional content, if needed -->
-            <a @click="navigateToSection">
-              <button
-                class="bg-sky-400 text-white px-3 py-3 rounded-full text-sm hover:bg-sky-600"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-6 h-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M12 4.5v15m7.5-7.5h-15"
-                  />
-                </svg></button
-            ></a>
           </ServiceCard>
         </div>
         <div class="flex flex-col items-center gap-4 mt-6 text-center">
